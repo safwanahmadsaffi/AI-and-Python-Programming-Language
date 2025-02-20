96fdfad2e721c2caa08fdeae19a072271fd84f50
safwaahmad <sahmedsaffi@gmail.com>
2025-02-20 10:43:02 +0500

    feat: add recursive display function and a simple multiplication function; update Jupyter notebook metadata

--- 09_function.py
+++ 09_function.py
@@ -6,3 +6,17 @@ gmean=(a*b)/(a+b)
 gmean1=(c*d)/(c+d)
 print(gmean)
 print(gmean1)
+def display(k):
+    if k > 0:   # Base Case
+        display(k - 1)
+        print(k)
+
+# Driver Code
+a = eval(input("\n\nEnter a Number:"))
+display(a)
+
+def my_function(x):
+  return 5 * x
+print(my_function(3))
+print(my_function(5))
+print(my_function(9)) 
